71210dfcc113b21a9003551490811c1a33884d9b
KuiMing <benjamin0901@gmail.com>
2023-07-05 11:11:42 +0800

    select data to predict the score and display shap result

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,74 +10,19 @@ import pandas as pd
 import streamlit as st
 from st_aggrid import AgGrid, GridOptionsBuilder
 from st_aggrid.shared import GridUpdateMode
+import pickle
+import numpy as np
+import shap
+
 """
 # 信用風險
 
 """
 
-st.info('This is a purely informational message', icon="ℹ️")
+st.info("This is a purely informational message", icon="ℹ️")
 
-# st.markdown(
-#     """
-#     <style>
-#     h1 {
-#         font-size: 20rem !important;
-#     }
-#     </style>
-#     # Head
-#     """,
-#     unsafe_allow_html=True,
-# )
-# with st.echo():
 
 cola, colb = st.columns([2, 1])
-total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
-Point = namedtuple('Point', 'x y')
-data = []
-
-points_per_turn = total_points / num_turns
-
-for curr_point_num in range(total_points):
-    curr_turn, i = divmod(curr_point_num, points_per_turn)
-    angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-    radius = curr_point_num / total_points
-    x = radius * math.cos(angle)
-    y = radius * math.sin(angle)
-    data.append(Point(x, y))
-df = pd.DataFrame(data)
-
-# with cola:
-#     Point = namedtuple('Point', 'x y')
-#     data = []
-
-#     points_per_turn = total_points / num_turns
-
-#     for curr_point_num in range(total_points):
-#         curr_turn, i = divmod(curr_point_num, points_per_turn)
-#         angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-#         radius = curr_point_num / total_points
-#         x = radius * math.cos(angle)
-#         y = radius * math.sin(angle)
-#         data.append(Point(x, y))
-
-#     # st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-#     #     .mark_circle(color='#0068c9', opacity=0.5)
-#     #     .encode(x='x:Q', y='y:Q'))
-
-#     df = pd.DataFrame(data)
-#     fig = px.scatter(df,x="x",y="y",
-#                     width=200, height=500)
-#     fig.update_traces(opacity=.4)
-#     tab1, tab2 = st.tabs(["Streamlit theme (default)", "Plotly native theme"])
-
-#     with tab1:
-#         # Use the Streamlit theme.
-#         # This is the default. So you can also omit the theme argument.
-#         st.plotly_chart(fig, theme="streamlit", use_container_width=True)
-#     with tab2:
-#         # Use the native Plotly theme.
-#         st.plotly_chart(fig, theme=None, use_container_width=True)
 
 
 def aggrid_interactive_table(df: pd.DataFrame):
@@ -96,6 +41,9 @@ def aggrid_interactive_table(df: pd.DataFrame):
     options.configure_side_bar()
 
     options.configure_selection("single")
+    options.configure_pagination(
+        enabled=True, paginationAutoPageSize=True, paginationPageSize=10
+    )
     selection = AgGrid(
         df,
         enable_enterprise_modules=True,
@@ -107,24 +55,78 @@ def aggrid_interactive_table(df: pd.DataFrame):
 
     return selection
 
-with cola:
-    iris = pd.read_csv("/Users/chenkuiming/Downloads/Credit_score/train.csv", nrows=10)
-    selection = aggrid_interactive_table(df=iris)
 
-    if selection:
-        st.write("You selected:")
-        try:
-            st.json(dict(Name=selection["selected_rows"][0]["Name"]))
-        except:
-            st.json([])
+with open("model.pkl", "rb") as f:
+    model = pickle.load(f)
+f.close()
+cols = [
+    "Annual_Income",
+    "Monthly_Inhand_Salary",
+    "Num_Bank_Accounts",
+    "Num_Credit_Card",
+    "Interest_Rate",
+    "Num_of_Loan",
+    "Delay_from_due_date",
+    "Num_of_Delayed_Payment",
+    "Credit_Mix",
+    "Outstanding_Debt",
+    "Credit_History_Age",
+    "Monthly_Balance",
+]
+cols_ch = [
+    "年薪",
+    "月薪",
+    "銀行帳戶數",
+    "信用卡張數",
+    "利息",
+    "信貸數量",
+    "遲交天數",
+    "遲交次數",
+    "混合信用分數",
+    "未償債務",
+    "信用紀錄月數",
+    "每月餘額",
+]
+test_data = pd.DataFrame(
+    np.array(
+        [
+            -1.10399851e-02,
+            -8.04193807e-04,
+            -1.48516614e-02,
+            -8.07955399e-03,
+            -4.98199064e-02,
+            -1.97553948e-02,
+            -6.60816043e-02,
+            5.58308875e-03,
+            -8.72436039e-03,
+            -6.40038640e-02,
+            -2.16789976e-02,
+            8.16568990e-05,
+        ]
+    ).reshape(1, -1),
+    columns=cols_ch,
+)
+
+with cola:
+    data = pd.read_csv("/Users/chenkuiming/Downloads/Credit_score/train.csv", nrows=10)
+    data = data[cols]
+
+    data["Credit_Mix"] = data["Credit_Mix"].map({"Standard": 1, "Good": 2, "Bad": 0})
+    data.columns = cols_ch
+    selection = aggrid_interactive_table(df=data)
+
+    # if selection:
+    #     st.write("You selected:")
+    #     try:
+    #         st.json(dict(Name=selection["selected_rows"][0]["Name"]))
+    #     except:
+    #         st.json([])
 
 with colb:
-
-
     style = """
     <style>
         .container { 
-        height: 500px;
+        height: 400px;
         position: relative;
         border: 3px solid green; 
         }
@@ -138,27 +140,57 @@ with colb:
         }
     </style>
     """
+
     try:
-        score = selection["selected_rows"][0]["Credit_Score"]
+        test_data = selection["selected_rows"][0]
+        test_data.pop("_selectedRowNodeInfo")
+        test_data = np.array(list(test_data.values()))
+        score = model.predict(test_data.reshape(1, -1))[0]
+        test_data = pd.DataFrame(test_data.reshape(1, -1), columns=data.columns)
     except:
-        score = ""
-    st.markdown("""
+        score = "Good"
+    st.markdown(
+        """
     {1}
     <div class="container">
         <h1 class="vertical-center" style='text-align: center; color: grey;'>Credit Score: {0}</h1>
     </div>
-    """.format(score, style), unsafe_allow_html=True)
+    """.format(
+            score, style
+        ),
+        unsafe_allow_html=True,
+    )
 
-
-col1, col2 = st.columns(2)
+st.markdown("---")
+col1, col2 = st.columns([2, 3])
 
 with col1:
-    table = go.Figure(data=[go.Table(header=dict(values=['A Scores', 'B Scores']),
-                    cells=dict(values=[[100, 90, 80, 90], [95, 85, 75, 95]]))
-                        ])
-    st.plotly_chart(table, theme="streamlit", use_container_width=True)
+    text = "#### 該交易的各項相關數值\n"
+    for i in test_data.columns:
+        text += f"- {i}: {test_data[i].values[0]}\n"
+    st.markdown(text)
 
+
+shap.initjs()
+explainer = shap.TreeExplainer(model)
 
 with col2:
-   fig = px.histogram(df, x="x")
-   st.plotly_chart(fig, theme="streamlit", use_container_width=True)
+    try:
+        # Compute SHAP values for the random person
+        shap_values = explainer.shap_values(test_data)
+        print(shap_values[1][0])
+        fig = []
+        fig.append(
+            go.Bar(
+                y=test_data.columns,
+                x=shap_values[list(model.classes_).index(score)][0],
+                name=score,
+                orientation="h",
+            )
+        )
+
+        figure = go.Figure(fig)
+        figure.update_layout(title="影響該交易信用風險的因子")
+        st.plotly_chart(figure, theme="streamlit", use_container_width=True)
+    except:
+        pass
