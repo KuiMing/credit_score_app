4767e1c0235080bed7a628c539103f8f0d79297b
ChunJu Hsiao <cjhsiao@ChunJus-MacBook-Air.local>
2023-07-08 19:01:07 +0800

    Change table content and using altair to plot bar chart

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,70 +1,44 @@
-from collections import namedtuple
 import altair as alt
-import math
 import pandas as pd
 import streamlit as st
-import plotly.figure_factory as ff
-import plotly.express as px
-import plotly.graph_objects as go
-import pandas as pd
-import streamlit as st
-from st_aggrid import AgGrid, GridOptionsBuilder
-from st_aggrid.shared import GridUpdateMode
+from st_aggrid import AgGrid, GridOptionsBuilder, ColumnsAutoSizeMode
 import pickle
-import numpy as np
 import shap
 
-"""
-# 信用風險
+st.set_page_config(layout="wide")
 
-"""
-
-
-cola, colb = st.columns([2, 1])
-
-
-def aggrid_interactive_table(df: pd.DataFrame):
-    """Creates an st-aggrid interactive table based on a dataframe.
-
-    Args:
-        df (pd.DataFrame]): Source dataframe
-
-    Returns:
-        dict: The selected row
-    """
-    options = GridOptionsBuilder.from_dataframe(
-        df, enableRowGroup=True, enableValue=True, enablePivot=True
-    )
-
-    options.configure_side_bar()
-
-    options.configure_selection("single")
-
-    selection = AgGrid(
-        df,
-        enable_enterprise_modules=True,
-        gridOptions=options.build(),
-        # theme="dark",
-        update_mode=GridUpdateMode.MODEL_CHANGED,
-        allow_unsafe_jscode=True,
-        height=350,
-    )
-
-    return selection
-
-
-credit = dict(Good="低", Standard="中", Poor="高")
+data = pd.read_csv("train.csv")
+data["Gender"] = data["Name"].apply(lambda n: 'Male' if hash(n)%2==0 else 'Female')
 
 with open("model.pkl", "rb") as f:
     model = pickle.load(f)
 f.close()
-cols = [
+col_translate = {
+    "Customer_ID":"客戶編號",
+    "Name":"姓名",
+    "Gender":"性別",
+    "Age":"年齡",
+    "Annual_Income": "年薪",
+    "Monthly_Inhand_Salary": "月薪",
+    "Num_Bank_Accounts": "銀行帳戶數",
+    "Num_Credit_Card": "信用卡張數",
+    "Interest_Rate": "信貸最高利息",
+    "Num_of_Loan": "貸款數量",
+    "Delay_from_due_date": "延遲還款天數",
+    "Num_of_Delayed_Payment": "延遲還款數量",
+    "Credit_Mix": "混合信用分數",
+    "Outstanding_Debt": "未償債務",
+    "Credit_History_Age": "信用紀錄月數",
+    "Monthly_Balance": "每月結餘",
+}
+
+predict_col = [
     "Annual_Income",
     "Monthly_Inhand_Salary",
     "Num_Bank_Accounts",
     "Num_Credit_Card",
-    "Interest_Rate",
     "Num_of_Loan",
+    "Interest_Rate",
     "Delay_from_due_date",
     "Num_of_Delayed_Payment",
     "Credit_Mix",
@@ -72,48 +46,32 @@ cols = [
     "Credit_History_Age",
     "Monthly_Balance",
 ]
-cols_ch = [
-    "年薪",
-    "月薪",
-    "銀行帳戶數",
-    "信用卡張數",
-    "利息",
-    "信貸數量",
-    "遲交天數",
-    "遲交次數",
-    "混合信用分數",
-    "未償債務",
-    "信用紀錄月數",
-    "每月餘額",
-]
-test_data = pd.DataFrame(
-    np.array(
-        [
-            -1.10399851e-02,
-            -8.04193807e-04,
-            -1.48516614e-02,
-            -8.07955399e-03,
-            -4.98199064e-02,
-            -1.97553948e-02,
-            -6.60816043e-02,
-            5.58308875e-03,
-            -8.72436039e-03,
-            -6.40038640e-02,
-            -2.16789976e-02,
-            8.16568990e-05,
-        ]
-    ).reshape(1, -1),
-    columns=cols_ch,
-)
 
-with cola:
-    data = pd.read_csv("train.csv", nrows=30)
-    data = data[cols]
+def aggrid_interactive_table(df: pd.DataFrame):
+    options = GridOptionsBuilder.from_dataframe(
+        df, enableRowGroup=True, enableValue=True, enablePivot=True
+    )
+    options.configure_selection("single")
+    selection = AgGrid(
+        df.round(),
+        gridOptions=options.build(),
+        enable_enterprise_modules=True,
+        columns_auto_size_mode=ColumnsAutoSizeMode.FIT_ALL_COLUMNS_TO_VIEW,
+        suppressColumnVirtualisation=True,
+        allow_unsafe_jscode=True,
+        height=350,
+    )
+    return selection
 
-    data["Credit_Mix"] = data["Credit_Mix"].map({"Standard": 1, "Good": 2, "Bad": 0})
-    data.columns = cols_ch
-    selection = aggrid_interactive_table(df=data)
+table_view, credit_score_view = st.columns([2, 1])
 
+with table_view:
+    
+    table_data = data[["Customer_ID","Name","Gender","Age","Annual_Income","Outstanding_Debt","Monthly_Balance"]]
+    table_data = table_data.groupby(["Customer_ID","Name","Gender"]).mean().reset_index().round()
+    table_data.columns = [col_translate[col] for col in table_data.columns]
+    
+    selection = aggrid_interactive_table(df=table_data)
     # if selection:
     #     st.write("You selected:")
     #     try:
@@ -121,13 +79,14 @@ with cola:
     #     except:
     #         st.json([])
 
-with colb:
+
+
+with credit_score_view:
     style = """
     <style>
         .container { 
         height: 400px;
         position: relative;
-        border: 3px solid green; 
         }
 
         .vertical-center {
@@ -139,15 +98,16 @@ with colb:
         }
     </style>
     """
-
+    credit = dict(Good="低", Standard="中", Poor="高", Not_Selected="<br>請選擇一名客戶")
     try:
-        test_data = selection["selected_rows"][0]
-        test_data.pop("_selectedRowNodeInfo")
-        test_data = np.array(list(test_data.values()))
-        score = model.predict(test_data.reshape(1, -1))[0]
-        test_data = pd.DataFrame(test_data.reshape(1, -1), columns=data.columns)
+        selected_user_id = selection["selected_rows"][0]["客戶編號"]
+        selected_data = data.loc[data['Customer_ID'] == selected_user_id,predict_col].head(1)
+        predict_data = selected_data.copy()
+        predict_data["Credit_Mix"] = predict_data["Credit_Mix"].map({"Standard": 1, "Good": 2, "Bad": 0})
+        score = model.predict(predict_data.to_numpy())[0]
+    # selected_data = pd.DataFrame(selected_data.reshape(1, -1), columns=data.columns
     except:
-        score = "Poor"
+        score = "Not_Selected"
     st.markdown(
         """
     {1}
@@ -160,42 +120,41 @@ with colb:
         unsafe_allow_html=True,
     )
 
-st.markdown("---")
-col1, col2 = st.columns([2, 3])
+# st.markdown("---")
+user_detail, score_sorting = st.columns([1, 3])
 
-with col1:
-    text = "#### 該交易的各項相關數值\n"
-    for i in test_data.columns:
-        text += f"- {i}: {test_data[i].values[0]}\n"
-    st.markdown(text)
+with user_detail:
+    try:
+        text = "#### 該客戶的財務相關資訊：\n"
+        selected_data.columns = [col_translate[col] for col in selected_data.columns]
+        for i in selected_data.columns:
+            text += f"- {i}: {selected_data[i].values[0]}\n"
+        st.markdown(text)
+    except:
+        pass
 
 
 shap.initjs()
 explainer = shap.TreeExplainer(model)
 
-with col2:
+with score_sorting:
     try:
         # Compute SHAP values for the random person
-        shap_values = explainer.shap_values(test_data)
-        test_df = pd.DataFrame(
+        shap_values = explainer.shap_values(predict_data)
+        selected_factor = pd.DataFrame(
             {
-                "factor": test_data.columns,
+                "factor": selected_data.columns,
                 "importance": shap_values[list(model.classes_).index(score)][0],
+                "value": selected_data.values[0],
             }
         )
-        test_df.sort_values(by="importance", inplace=True)
-        fig = []
-        fig.append(
-            go.Bar(
-                y=test_df.factor,
-                x=test_df.importance,
-                name=score,
-                orientation="h",
-            )
-        )
 
-        figure = go.Figure(fig)
-        figure.update_layout(title="影響該交易信用風險的因子")
-        st.plotly_chart(figure, theme="streamlit", use_container_width=True)
+        c = alt.Chart(selected_factor, mark='bar').encode(
+            x=alt.X('factor', sort=alt.EncodingSortField(field='importance', order='descending')), 
+            y='importance',
+            tooltip=['factor', 'importance','value']
+            ).configure_axisX(labelAngle=0)
+        st.markdown("#### 影響信用風險的因子佔比")
+        st.altair_chart(c, use_container_width=True)
     except:
         pass
